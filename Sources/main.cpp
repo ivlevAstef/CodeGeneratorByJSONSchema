0704c631e706409cb94991654acf3fda4b95688a
ivlevAstef <ivlev.stef@gmail.com>
2016-01-13 12:49:25 +0600

    Added project and clang format.

---
+++ Sources/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CodeGeneratorByJSONScheme
+//
+//  Created by Alexander.Ivlev on 1/13/16.
+//  Copyright © 2016 themsteam. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char* argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
